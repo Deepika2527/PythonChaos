d3fd1ce13b8c2df52046e963d4c7b99c3cf8d8c7
Deepika2527 <deepikatrainings.com@gmail.com>
2026-02-09 17:21:50 +0530

    Day3_Variables|Indentation|Comments|Identifier

---
+++ Day3_Identifers_Variables/demo.py
@@ -0,0 +1,74 @@
+print('Hello')
+
+name = 'nikath'
+print(name)
+
+# $age = 20
+# print($age)
+
+course = 'python'
+Course = 'Python'
+
+print(course)
+print(Course)
+
+
+_firstname = 'deepika'
+print(_firstname)
+
+
+class1  = 'react'
+print(class1)
+
+# how to access keywords
+import keyword
+print(keyword.kwlist)
+
+
+a,b,c = 10, 20, 30
+print(a)
+print(b)
+print(c)
+
+
+x = y = z = 1000
+print(x)
+print(y)
+print(z)
+
+
+
+def greet():
+    print('Hello')
+
+greet()
+
+
+class Student:
+    pass
+
+
+
+def disp():
+    '''This is generally used for refernce which is ignored and cant be printed '''
+    print('Exanmple from comment')
+disp()
+
+
+def display():
+    print('hello')
+display()
+
+
+age = 20
+if(age>18):
+    if(age<60):
+        print('There are eligible for work')
+
+
+for i in range(3):
+    for j in range(2):
+        print(f"i= {i}, j= {j}")
+
+
+        
